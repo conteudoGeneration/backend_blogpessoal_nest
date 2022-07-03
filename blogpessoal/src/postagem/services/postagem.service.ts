ee24d6d8547a252748149c11841f74adffc0a3d3
Rafael <rafaelproinfo@gmail.com>
2022-07-03 14:29:47 -0300

    Update PostagemService

--- blogpessoal/src/postagem/services/postagem.service.ts
+++ blogpessoal/src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { Postagem } from "../Entities/postagem.entity";
+import { Postagem } from "../entities/postagem.entity";
 
 @Injectable()
 export class PostagemService {
