192a96688bc44b7088e565042e96ed1b04781856
Rafael <rafaelproinfo@gmail.com>
2022-07-03 14:39:54 -0300

    Update PostagemService

--- blogpessoal/src/app.module.ts
+++ blogpessoal/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/Entities/postagem.entity';
+import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 
 @Module({
