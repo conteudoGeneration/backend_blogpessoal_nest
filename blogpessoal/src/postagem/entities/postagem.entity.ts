1d67fd0ec6580ffd12f4707b48f8d08d118e6ff3
Rafael <rafaelproinfo@gmail.com>
2022-07-12 18:09:27 -0300

    Update Entitdade

--- blogpessoal/src/postagem/entities/postagem.entity.ts
+++ blogpessoal/src/postagem/entities/postagem.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn, Tree, UpdateDateColumn } from "typeorm"
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 
 @Entity({name: "tb_postagens"})
 export class Postagem {
@@ -6,9 +7,11 @@ export class Postagem {
     @PrimaryGeneratedColumn()    
     id: number
 
+    @IsNotEmpty()
     @Column({length: 100, nullable: false})
     titulo: string
 
+    @IsNotEmpty()
     @Column({length: 1000, nullable: false})
     texto: string
 
@@ -16,5 +19,3 @@ export class Postagem {
     data: Date
     
 }
-
-
