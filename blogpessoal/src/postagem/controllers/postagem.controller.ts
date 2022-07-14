6a457eb7593fe7cb7d7ea46d52978a3449af2160
Rafael <rafaelproinfo@gmail.com>
2022-07-14 00:26:39 -0300

    Update PostagemController

--- blogpessoal/src/postagem/controllers/postagem.controller.ts
+++ blogpessoal/src/postagem/controllers/postagem.controller.ts
@@ -4,42 +4,42 @@ import { PostagemService } from "../services/postagem.service";
 
 @Controller("/postagens")
 export class PostagemController {
-  constructor(private readonly service: PostagemService) { }
+  constructor(private readonly postagemService: PostagemService) { }
 
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
-    return this.service.findAll();
+    return this.postagemService.findAll();
   }
 
   @Get('/:id')
   @HttpCode(HttpStatus.OK)
   findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
-    return this.service.findById(id);
+    return this.postagemService.findById(id);
   }
 
   @Get('/titulo/:titulo')
   @HttpCode(HttpStatus.OK)
   findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
-    return this.service.findByTitulo(titulo);
+    return this.postagemService.findByTitulo(titulo);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() postagem: Postagem): Promise<Postagem> {
-    return this.service.create(postagem);
+    return this.postagemService.create(postagem);
   }
 
   @Put()
   @HttpCode(HttpStatus.OK)
   update(@Body() postagem: Postagem): Promise<Postagem> {
-    return this.service.update(postagem);
+    return this.postagemService.update(postagem);
   }
 
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id', ParseIntPipe) id: number){
-    return this.service.delete(id);
+    return this.postagemService.delete(id);
   }
 
 }
