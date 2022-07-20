c5b57b6c48ac2ac63fe8e86ab478dbb1a0b6f442
Rafael <rafaelproinfo@gmail.com>
2022-07-20 18:00:13 -0300

    Update Usuario

--- blogpessoal/src/auth/controllers/auth.controller.ts
+++ blogpessoal/src/auth/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import { UsuarioLogin } from './../entities/usuariologin.entity';
 import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../guard/local-auth.guard';
 import { AuthService } from '../services/auth.service';
@@ -9,7 +10,7 @@ export class AuthController {
     @UseGuards(LocalAuthGuard)
     @HttpCode(HttpStatus.OK)
     @Post('/logar')
-    async login(@Body() user: any): Promise<any> {
+    async login(@Body() user: UsuarioLogin): Promise<any> {
         return this.authService.login(user);
     }
 
