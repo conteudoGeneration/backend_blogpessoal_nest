c872981b36fa658da8a3df6e43d838f4c6936463
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-01-20 00:43:27 -0300

    Update Model Usuario

--- blogpessoal/src/usuario/entities/usuario.entity.ts
+++ blogpessoal/src/usuario/entities/usuario.entity.ts
@@ -1,30 +1,31 @@
-import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
-import { Postagem } from "../../postagem/entities/postagem.entity"
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Postagem } from "../../postagem/entities/postagem.entity";
 
-@Entity({ name: "tb_usuarios" })
-export class Usuario {
+@Entity({name: "tb_usuarios"})
+export class Usuario{
 
     @PrimaryGeneratedColumn()
     public id: number
 
     @IsNotEmpty()
-    @Column({ length: 255, nullable: false })
+    @Column({length: 255, nullable: false})
     public nome: string
 
     @IsEmail()
-    @Column({ length: 255, nullable: false })
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
     public usuario: string
 
-    @IsNotEmpty()
     @MinLength(8)
-    @Column({ length: 255, nullable: false })
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
     public senha: string
 
-    @Column({ length: 5000 })
+    @Column({length: 5000})
     public foto: string
 
     @OneToMany(() => Postagem, (postagem) => postagem.usuario)
     postagem: Postagem[]
-
+    
 }
