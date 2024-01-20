3749768a1ae5c5dc7516b7a11ad73435d2a7f6c2
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-01-20 12:50:43 -0300

    Update usuario.controller.ts

--- blogpessoal/src/usuario/controllers/usuario.controller.ts
+++ blogpessoal/src/usuario/controllers/usuario.controller.ts
@@ -1,27 +1,35 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Put } from "@nestjs/common";
-import { Usuario } from "../entities/usuario.entity";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { UsuarioService } from "../services/usuario.service";
+import { Usuario } from "../entities/usuario.entity";
+
 
 @Controller("/usuarios")
-export class UsuarioController {
-    constructor(private readonly usuarioService: UsuarioService) { }
+export class UsuarioController{
+
+    constructor(private readonly usuarioService: UsuarioService){ }
 
     @Get('/all')
     @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Usuario[]> {
+    findAll(): Promise<Usuario[]>{
         return this.usuarioService.findAll();
     }
 
-    @HttpCode(HttpStatus.CREATED)
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Usuario>{
+        return this.usuarioService.findById(id)
+    }
+
     @Post('/cadastrar')
-    async create(@Body() usuario: Usuario): Promise<Usuario> {
-        return await this.usuarioService.create(usuario);
+    @HttpCode(HttpStatus.CREATED)
+    async create(@Body() usuario: Usuario): Promise<Usuario>{
+        return this.usuarioService.create(usuario)
     }
 
     @Put('/atualizar')
     @HttpCode(HttpStatus.OK)
-    async update(@Body() usuario: Usuario): Promise<Usuario> {
-        return this.usuarioService.update(usuario);
+    async update(@Body() usuario: Usuario): Promise<Usuario>{
+        return this.usuarioService.update(usuario)
     }
 
 }
