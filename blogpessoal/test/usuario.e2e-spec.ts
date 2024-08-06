b7bdf90a57f591e73d8bd4573e052534839fe585
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-08-05 23:58:50 -0300

    Update usuario.e2e-spec.ts

--- blogpessoal/test/usuario.e2e-spec.ts
+++ blogpessoal/test/usuario.e2e-spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
+describe('Testes dos Módulos Usuario e Auth (e2e)', () => {
 
   let token: any;
   let usuarioId: any;
@@ -14,31 +14,32 @@ describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         TypeOrmModule.forRoot({
-          type: "sqlite",
-          database: ":memory:",
+          type: 'sqlite',
+          database: ':memory:',
           entities: [__dirname + "./../src/**/entities/*.entity.ts"],
           synchronize: true,
-          dropSchema: true
+          dropSchema: true,
         }),
         AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
   afterAll(async () => {
     await app.close();
-  });
+  })
 
-  it('01 - Deve Cadastrar Usuario', async () => {
+  it("01 - Deve Cadastrar um novo Usuário", async () => {
     const resposta = await request(app.getHttpServer())
       .post('/usuarios/cadastrar')
       .send({
         nome: 'Root',
         usuario: 'root@root.com',
         senha: 'rootroot',
-        foto: ' '
+        foto: '-',
       })
       .expect(201)
 
@@ -46,54 +47,56 @@ describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
 
   });
 
-  it('02 - Não Deve Duplicar o Usuário', async () => {
-    return request(app.getHttpServer())
+  it("02 - Não Deve Cadastrar um Usuário Duplicado", async () => {
+    await request(app.getHttpServer())
       .post('/usuarios/cadastrar')
       .send({
         nome: 'Root',
         usuario: 'root@root.com',
         senha: 'rootroot',
-        foto: ' '
+        foto: '-',
       })
       .expect(400)
+
   });
 
-  it('03 - Deve Autenticar Usuario (Login)', async () => {
+  it("03 - Deve Autenticar o Usuário (Login)", async () => {
     const resposta = await request(app.getHttpServer())
-      .post('/usuarios/logar')
-      .send({
-        usuario: 'root@root.com',
-        senha: 'rootroot',
-      })
-      .expect(200)
+    .post("/usuarios/logar")
+    .send({
+      usuario: 'root@root.com',
+      senha: 'rootroot',
+    })
+    .expect(200)
 
     token = resposta.body.token;
 
-  });
+  })
 
-  it('04 - Deve Listar todos os Usuários', async () => {
+  it("04 - Deve Listar todos os Usuários", async () => {
     return request(app.getHttpServer())
-      .get('/usuarios/all')
-      .set('Authorization', `${token}`)
-      .send({})
-      .expect(200)
-  });
+    .get('/usuarios/all')
+    .set('Authorization', `${token}`)
+    .send({})
+    .expect(200)
+  })
 
-  it('05 - Deve Atualizar um Usuário', async () => {
+  it("05 - Deve Atualizar um Usuário", async () => {
     return request(app.getHttpServer())
-      .put('/usuarios/atualizar')
-      .set('Authorization', `${token}`)
-      .send({
-        id: usuarioId,
-        nome: 'Root Atualizado',
-        usuario: 'root@root.com',
-        senha: 'rootroot',
-        foto: ' '
-      })
-      .expect(200)
-      .then(resposta => {
-        expect("Root Atualizado").toEqual(resposta.body.nome);
-      });
-  });
+    .put('/usuarios/atualizar')
+    .set('Authorization', `${token}`)
+    .send({
+      id: usuarioId,
+      nome: 'Root Atualizado',
+      usuario: 'root@root.com',
+      senha: 'rootroot',
+      foto: '-',
+    })
+    .expect(200)
+    .then( resposta => {
+      expect("Root Atualizado").toEqual(resposta.body.nome);
+    })
+
+  })
 
 });
